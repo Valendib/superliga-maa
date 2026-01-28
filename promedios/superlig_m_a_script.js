f680eb3a9e95c68aeeb84f2c295f99e08277335f
Valendib <valendiberti@gmail.com>
2026-01-28 10:04:32 -0300

    superlig_m_a_script.js

--- promedios/superlig_m_a_script.js
+++ promedios/superlig_m_a_script.js
@@ -1,29 +1,6 @@
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    const tabla = document.createElement("table");
-    tabla.border = "1";
-    tabla.style.borderCollapse = "collapse";
-
-    // Encabezado
-    const header = document.createElement("tr");
-    header.innerHTML = "<th>Equipo</th><th>Promedio</th>";
-    tabla.appendChild(header);
-
-    // Filas
-    data.forEach(item => {
-      const fila = document.createElement("tr");
-      fila.innerHTML = `
-        <td>${item.equipo}</td>
-        <td>${item.promedio}</td>
-      `;
-      tabla.appendChild(fila);
-    });
-
-    document.body.appendChild(tabla);
-  })
-  .catch(error => {
-    document.body.innerHTML += "<p>Error cargando datos</p>";
-    console.error(error);
-  });
-
+[
+  { "equipo": "Carita F.C.", "promedio": 1.50 },
+  { "equipo": "Guris HC", "promedio": 1.32 },
+  { "equipo": "Racing VN", "promedio": 1.10 },
+  { "equipo": "Real Patapim", "promedio": 0.95 }
+]
